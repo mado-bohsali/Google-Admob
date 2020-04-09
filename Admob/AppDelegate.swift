da0e11cb8d1118383d2ddc7a20d9884452a89528
mado-bohsali <mohamad.elbohsaly@gmail.com>
2020-04-09 10:54:35 +0300

    Complete Banner Ad programmatically

--- Admob/AppDelegate.swift
+++ Admob/AppDelegate.swift
@@ -7,14 +7,18 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        
+        //Before calling ads: initialize SDK
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
         return true
     }
 
--- Admob/ViewController.swift
+++ Admob/ViewController.swift
@@ -3,18 +3,81 @@
 //  Admob
 //
 //  Created by Mohamad El Bohsaly on 4/9/20.
-//  Copyright © 2020 Mohamad El Bohsaly. All rights reserved.
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
-
+    var banner: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Ads by Google of type Banner - Intersistal - Rewarded
+        banner = GADBannerView(adSize: kGADAdSizeBanner)
+        addBannerToView(banner)
+        
+        banner.adUnitID = "ca-app-pub-7142967589737903/2607835245"
+        banner.rootViewController = self
+        
+        
+    }
+    
+    // MARK: Ads configuration (programmatically)
+    func addBannerToView(_ bannerView: GADBannerView){
+        bannerView.translatesAutoresizingMaskIntoConstraints = false //intrinsic content size to size the view
+        view.addSubview(bannerView)
+        view.addConstraints([NSLayoutConstraint(item: bannerView, attribute: .bottom, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0),
+                             NSLayoutConstraint(item: bannerView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)])
     }
 
+    @IBAction func bannerTriggered(_ sender: UIButton) {
+        //properties placed and configured, then load ad
+        banner.load(GADRequest())
+    }
+    
+    @IBAction func interstitalTriggered(_ sender: UIButton) {
+    }
+    
+    @IBAction func rewardedTriggered(_ sender: Any) {
+        
+    }
+    
+}
 
+extension ViewController: GADBannerViewDelegate {
+    /// Tells the delegate an ad request loaded an ad.
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+      print("adViewDidReceiveAd")
+    }
+
+    /// Tells the delegate an ad request failed.
+    func adView(_ bannerView: GADBannerView,
+        didFailToReceiveAdWithError error: GADRequestError) {
+      print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+
+    /// Tells the delegate that a full-screen view will be presented in response
+    /// to the user clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+      print("adViewWillPresentScreen")
+    }
+
+    /// Tells the delegate that the full-screen view will be dismissed.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+      print("adViewWillDismissScreen")
+    }
+
+    /// Tells the delegate that the full-screen view has been dismissed.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+      print("adViewDidDismissScreen")
+    }
+
+    /// Tells the delegate that a user click will open another app (such as
+    /// the App Store), backgrounding the current app.
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+      print("adViewWillLeaveApplication")
+    }
 }
 
