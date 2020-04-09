2bc3cc0528308d3a2ab5ad9cb7f9b97ec78a367c
mado-bohsali <mohamad.elbohsaly@gmail.com>
2020-04-09 10:01:45 +0300

    Initial Commit

---
+++ Admob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Admob
+//
+//  Created by Mohamad El Bohsaly on 4/9/20.
+//  Copyright © 2020 Mohamad El Bohsaly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
